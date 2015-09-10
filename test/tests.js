c2ef73581698737e69d8fb7cfaf40e275c2bc7dd
thejsj <jorge.silva@thejsj.com>
2015-09-10 16:41:54 -0700

    Add 2 test cases for apt-get options

--- test/tests.js
+++ test/tests.js
@@ -226,6 +226,14 @@ describe('Packages', function () {
     var packages = new Packages(preamble + packageList + ' -y -q apt-get update && apt-get upgrade --force-yes');
     expect(packages.toString()).to.equal('#Start: Packages\n'+preamble + packageList+'\n#End');
   });
+  it('should exclude some unique strings joined togeher', function () {
+    var packages = new Packages(preamble + packageList + ' -yq apt-get update && apt-get upgrade --force-yes');
+    expect(packages.toString()).to.equal('#Start: Packages\n'+preamble + packageList+'\n#End');
+  });
+  it('should exclude some longer versions of apt-get options, no matter the order', function () {
+    var packages = new Packages(preamble + packageList + '--yes --assume-yes --quiet apt-get update && apt-get upgrade --force-yes');
+    expect(packages.toString()).to.equal('#Start: Packages\n'+preamble + packageList+'\n#End');
+  });
 });
 
 describe('SSH Key', function () {
